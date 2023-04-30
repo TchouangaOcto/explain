dcb0d6233e6799839c3380de29f5e764373161d4
Franck Tchouanga <125889035+TchouangaOcto@users.noreply.github.com>
2023-05-01 01:06:52 +0200

    pusher ehhh

---
+++ Explanable/backend/load_donnée.py
@@ -0,0 +1,209 @@
+"""
+fichier permetant de chargé le fichier de donnée et mettre à jour le metadata donnée
+"""
+from dash import *
+import dask.dataframe as dd
+import datetime
+import psycopg2
+import pandas as pd
+import io
+import base64
+from backend.app import app
+import os
+import sys
+from pathlib import Path
+current_dir = os.getcwd()
+current_dir = Path(Path(current_dir).parent.absolute())
+print(current_dir)
+from log_app.log import log
+
+# instance de log
+file = "Explanable/log_app/backend.log"
+logfile = os.path.join(current_dir, file)
+logger = log()
+log = logger.log(logfile)
+
+filename = ''
+
+# css style
+style_header = {
+            'backgroundColor': '#96B9C4',
+            'color': '#F3FBFB',
+            'fontWeight': 'bold'
+        }
+
+style_header_conditional = [
+            {'if': {'column_id': 'id', },
+             'display': 'None', }]
+
+style_cell = {
+            'font-family': 'Overpass'
+        }
+
+style_data = {
+            'color': 'black',
+            'backgroundColor': 'white',
+            'whiteSpace': 'normal',
+            'height': 'auto',
+
+        }
+
+style_table = {'overflowX': 'auto'}
+
+style_data_conditional = [
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': '#EEEFF1',
+            },
+            {'if': {'column_id': 'id', },
+             'display': 'None', }
+        ]
+
+style_data_conditional2 = [
+            {
+                "if": {
+                    "state": "active"  # 'active' | 'selected'
+                },
+                "backgroundColor": "rgba(0, 116, 217, 0.3)",
+                "border": "1px solid rgb(0, 116, 217)",
+            },
+            {
+                "if": {
+                    "state": "selected"  # 'active' | 'selected'
+                },
+                "backgroundColor": "rgba(0, 116, 217, 0.3)",
+                "border": "1px solid rgb(0, 116, 217)",
+            },
+            {'if': {'column_id': 'id', },
+             'display': 'None', }
+        ]
+
+# layout
+layout = html.Div([
+                    dash_table.DataTable(
+                    id='table',
+                    row_selectable='single',
+                    page_size= 20,
+                    style_cell=style_cell,
+                    style_table=style_table,
+                    cell_selectable=True,
+                    style_header=style_header,
+                    style_data=style_data,
+                    style_data_conditional=style_data_conditional,
+                    style_header_conditional=style_header_conditional,
+                    virtualization=True
+                             ),])
+
+# connection à la base de donnée
+try:
+    log.info('connection avec le serveur postgres')
+    conn = psycopg2.connect(
+        database="postgres",
+        user='postgres',
+        password='0000',
+        host='localhost',
+        port='5432'
+    )
+except Exception as e:
+    log.error(e)
+
+def parse_fichier(contents, filename):
+        """
+        fonction récupérant le contenu du fichier chargés
+        :param contents: contenu du fichier chargé
+        :param filename: nom du fichier chargé
+        :return: un dictionaire du contenu charge si ce sont des données et rien du tout si c'est un modèle
+        """
+        content_type, content_string = contents.split(',')
+        decoded = base64.b64decode(content_string)
+        try:
+            if 'csv' in filename:
+                # pour les fichiers de type csv
+                log.info('le fichier à un format csv')
+                df = pd.read_csv(
+                    io.StringIO(decoded.decode('utf-8')))
+            elif 'xls' in filename:
+                # pour les fichiers de type xls
+                log.info('le fichier à un format xls')
+                df = pd.read_excel(io.BytesIO(decoded))
+            elif 'xlsx' in filename:
+                # pour les fichiers de type xlsx
+                log.info('le fichier à un format xlsx')
+                df = pd.read_excel(io.BytesIO(decoded))
+            elif 'pkl' in filename:
+                # pour les fichiers de type pkl
+                log.info('le fichier à un format pickle')
+                model = pd.read_pickle(io.BytesIO(decoded))
+                return no_update
+            candidates = ['Unnamed: 0']
+            df = df.drop([x for x in candidates if x in df.columns], 1)
+            df_table = df.copy()
+            df_table['id'] = df_table.index
+        except Exception as e:
+            log.error(e)
+        return df_table.to_dict('records'), df.columns, content_string
+
+def update_metadata(filename,contenu,date,sql):
+    """
+    fonction mettant à jour la table précisé dans la base de donnée
+    :param filename: nom du fichier de donnée chargé
+    :param date: date d'aujourd'hui
+    :param table: nom de la table
+    :return: None
+    """
+    try:
+        # connection à la base de donnée
+        log.info('connection avec le serveur postgres')
+        conn = psycopg2.connect(
+            database="postgres",
+            user='postgres',
+            password='0000',
+            host='localhost',
+            port='5432'
+        )
+    except Exception as e:
+        log.error(e)
+    cursor = conn.cursor()
+    values = (date,filename,contenu)
+    log.info('lancement du query')
+    cursor.execute(sql,values)
+    log.info('Arret du curseur de la base de donnée')
+    cursor.close()
+    log.info('query commité')
+    conn.commit()
+    log.info('ferméture de la connexion à la base de donnée')
+    conn.close()
+
+
+log.info('lancement du callback pour la gestion du data table')
+@app.callback(
+
+        Output('table', 'data'),
+        Input('donnée', 'contents'),
+        State('donnée', 'filename'),
+    )
+def update_app(contents_donnée, filename_donnée):
+        """
+        fonction mettant à jour une table de la base de donnée et lance le data table layout
+        :param contents_donnée:
+        :param filename_donnée:
+        :return: récupère le contenu du fichier donné et lance le data table layout
+        """
+        log.info('lancement de la fonction après le callback')
+        date = str(datetime.datetime.now().strftime(
+            "%Y-%m-%d %H:%M"))  # bon format de date (année , mois, jours, heure, minute)
+        if contents_donnée is not None:
+            log.info('mise à jours de table donnée')
+            table = 'metadata_donnée_v2'
+            sql = f"INSERT INTO {table} (date,fichier,contenu) VALUES (%s,%s,%s);"
+            log.info('chargement du contenu du fichier')
+            dict_donnée, _, content_string = parse_fichier(contents_donnée,
+                          filename_donnée)
+            log.info('création du data table')
+            update_metadata(filename_donnée,content_string, date, sql)  # mise à jour de la table metadata_donnée
+
+            return dict_donnée# récuperation du contenu du fichier de donnée et création du data table
+
+
+
+
---
+++ Explanable/backend/plot_explain_dash.py
@@ -0,0 +1,129 @@
+from dash import *
+import os
+import sys
+from pathlib import Path
+import dash_bootstrap_components as dbc
+from backend.app import app
+from backend.explain import *
+import psycopg2
+import pandas as pd
+import io
+import base64
+
+current_dir = os.getcwd()
+current_dir = Path(Path(current_dir).parent.absolute())
+print(current_dir)
+from log_app.log import log
+file = "Explanable/log_app/backend.log"
+logfile = os.path.join(current_dir, file)
+logger = log()
+log = logger.log(logfile)
+
+# variable clé pour lancer le modèle explicable de dalex
+
+card_style = {
+    "display": "inline-flex",
+    "padding": "2px 16px",
+    "position": "center",
+    "verticalAlign": "middle"
+}
+
+card_unique_style = {
+    "box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)",
+    "margin":"10px",
+    "width":"750px"
+}
+card_content = [
+    dbc.CardBody(
+        [
+            html.Div(id='graph_globale1'),
+        ]
+    ),
+]
+
+card_content2 = [
+    dbc.CardBody(
+        [
+            html.Div(id='graph_globale2'),
+        ]
+    ),
+]
+
+row_1 = dbc.Row(
+    [
+        dbc.Col(dbc.Card(card_content, color="primary", outline=True,style=card_unique_style),width=10),
+        dbc.Col(dbc.Card(card_content2, color="secondary", outline=True,style=card_unique_style),width=10),
+    ],style=card_style,
+    className="mb-4",
+)
+
+cards = html.Div([row_1])
+
+def load_explain(modèle,dataset, variable_predire):
+    dalex_explain = dalex(modèle, dataset, variable_predire)
+    return dalex_explain # retourne objet dalex
+
+def request(sql):
+    """
+    fonction mettant à jour la table précisé dans la base de donnée
+    :param filename: nom du fichier de donnée chargé
+    :param date: date d'aujourd'hui
+    :param table: nom de la table
+    :return: None
+    """
+    try:
+        # connection à la base de donnée
+        log.info('connection avec le serveur postgres')
+        conn = psycopg2.connect(
+            database="postgres",
+            user='postgres',
+            password='0000',
+            host='localhost',
+            port='5432'
+        )
+    except Exception as e:
+        log.error(e)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    valeurs = cursor.fetchall()
+    cursor.close()
+    log.info('ferméture de la connexion à la base de donnée')
+    conn.close()
+
+    return valeurs[0]
+@app.callback(
+
+    Output('graph_globale1', 'children'),
+    Output('graph_globale2', 'children'),
+    Input('checklist_explain', 'value'),
+    Input('Explanabilité_methdode', 'value'),
+    Input('variable_à_expliqué', 'value'),
+    Input('variable_explicatif', 'value')
+)
+
+def update_card(checkbox_valeur,dropdown_valeur,variable_predire, variable_explicatif):
+    if  'dalex' in dropdown_valeur  and 'Explanabilité :' in checkbox_valeur:
+        table1 ='metadata_donnée_v2'
+        table2 ='metadata_modèle'
+        sql =  f"select {table1}.contenu as data, {table2}.fichier as model, {table2}.contenu as contenu , max({table1}.date) as date from {table1}" \
+               f" left join {table2} on {table1}.date = {table2}.date where {table2}.fichier is not null group by data, " \
+               f"{table2}.fichier, {table2}.contenu order by date desc limit 1;"
+
+        dataset_contenu, modèle, modèle_contenu, _ = request(sql)
+        modèle_decoded = base64.b64decode(modèle_contenu)
+        dataset_decoded = base64.b64decode(dataset_contenu)
+        modèle = pd.read_pickle(io.BytesIO(modèle_decoded))
+        dataset = pd.read_csv(io.StringIO(dataset_decoded.decode('utf-8')))
+        dalex_explain = load_explain(modèle,dataset,variable_predire)
+        log.info(f"chargement de l'outil dalex {dalex_explain}")
+        #dalex_explain.feature_importance().update_traces(marker=dict(color="# 89D9D7"),hoverlabel=dict(bgcolor= "#F79034"),selector=dict(type="bar"))
+        #dalex_explain.feature_importance().data[0].hoverlabel.bgcolor =
+        log.info(f'graph info: {dalex_explain.predict_profile(variable_explicatif)}')
+        fig1 = dalex_explain.predict_profile(variable_explicatif).update_traces(marker=dict(color="#89D9D7"),
+                                                                hoverlabel=dict(bgcolor="#F79034"),
+                                                                selector=dict(type="line"))
+        fig1 = fig1.update_layout(font=dict(color='#013E50'))
+        fig2 = dalex_explain.feature_importance().update_traces(marker=dict(color="#89D9D7"),hoverlabel=dict(bgcolor= "#F79034"),selector=dict(type="bar"))
+        fig2 = fig2.update_layout(font=dict(color='#013E50'),annotations=dict(dict(text='')))
+        return dcc.Graph(figure=fig2), \
+            dcc.Graph(figure=fig1)
